c73d09e2bec89a3e0be0bfc5e2bfcd21b0705a1d
Md Salah Uddin <bdrosan@gmail.com>
2022-04-16 16:27:50 +0600

    category thumb size square

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -95,7 +95,7 @@ class CategoryController extends Controller
         ]);
         if ($success && $file_name) {
             Image::make($request->image)->save(public_path('storage/category/') . $file_name);
-            Image::make($request->image)->resize(320, 240)->save(public_path('storage/category/thumbs/') . $file_name);
+            Image::make($request->image)->resize(240, 240)->save(public_path('storage/category/thumbs/') . $file_name);
         }
         return response()->json([
             'success' => $success,
@@ -172,7 +172,7 @@ class CategoryController extends Controller
             $file_ex   = end($file);
             $file_name = uniqid() . '.' . $file_ex;
             Image::make($request->image)->save(public_path('storage/category/') . $file_name);
-            Image::make($request->image)->resize(320, 240)->save(public_path('storage/category/thumbs/') . $file_name);
+            Image::make($request->image)->resize(240, 240)->save(public_path('storage/category/thumbs/') . $file_name);
 
             if ($category->image) {
                 Storage::delete(['public/category/' . $category->image, 'public/category/thumbs/' . $category->image]);
